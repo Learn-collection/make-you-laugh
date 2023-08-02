6a85cb59f8aaf6ba46e00a394b674cdec11f2317
bunhor-deb <bunhor0303@gmail.com>
2023-08-02 23:52:25 +0700

    already add dynamic comedy content

--- script.js
+++ script.js
@@ -6,26 +6,29 @@ const btnOpenModal = document.querySelector('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 const comedyContent = document.querySelector('.comedy-content');
 
-const newComedyContent = "Here is new comedy content.";
-
-console.log(modal, btnOpenModal)
-
-const changeComedyContent = function(newComedyContent){
-    let text = comedyContent.textContent = newComedyContent;
-    console.log(text);
-};
-
+async function fetchJoke() {
+    const response = await fetch('https://icanhazdadjoke.com/', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    const data = await response.json();
+    return data.joke;
+}
+async function displayJoke() {
+const joke = await fetchJoke();
+comedyContent.textContent = joke;
+}
 const openModal = function (){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-    changeComedyContent(newComedyContent);
-};
+    displayJoke();
 
+};
 const closeModal = function (){
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
-
 btnOpenModal.addEventListener('click', openModal);
 btnCloseModal.addEventListener('click', closeModal);
 
